888d58ba1f296031ddb4b01088581bcdd664b270
awongh <akira@awongh.com>
2020-10-29 23:09:28 +0800

    add basic file server index.js & basic inde.html

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+import { createServer } from 'http';
+import { readFile } from 'fs';
+
+const PORT = 3004;
+
+const whenIncomingRequest = (request, response) => {
+  console.log('request url', request.url);
+
+  var filePath = '.' + request.url;
+
+  readFile(filePath, (error, content) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end(content, 'utf-8');
+  });
+};
+
+createServer(whenIncomingRequest).listen(PORT)
